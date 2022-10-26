df514eb6dd10be1f4c9511550b90928782dfb0f0
elfobella <yunusemreucr11@gmail.com>
2022-10-26 17:21:18 +0300

    Added grqphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplify003": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "imagestore": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
